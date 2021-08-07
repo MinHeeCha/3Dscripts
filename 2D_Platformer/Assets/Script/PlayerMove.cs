8f58624d8033b0c533b09eab04b576a8209f5f7a
MinHeeCha <69028785+MinHeeCha@users.noreply.github.com>
2021-08-07 17:11:15 +0900

    4 플레이어 점프 구현

--- 2D_Platformer/Assets/Script/PlayerMove.cs
+++ 2D_Platformer/Assets/Script/PlayerMove.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpPower;
+
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator anim;
@@ -22,7 +24,7 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetButtonUp("Horizontal"))
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
 
-        // Direction sprite
+        // Direction animation
         if (Input.GetButtonDown("Horizontal"))
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
 
@@ -31,6 +33,15 @@ public class PlayerMove : MonoBehaviour
             anim.SetBool("isWalking", false);
         else
             anim.SetBool("isWalking", true);
+
+        // Jump animation
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+        }
+        //else
+        //    anim.SetBool("isJumping", false);
 
     }
 
@@ -43,5 +54,22 @@ public class PlayerMove : MonoBehaviour
         // Max speed
         if (Mathf.Abs(rigid.velocity.x) > maxSpeed)
             rigid.velocity = new Vector2(rigid.velocity.x > 0 ? maxSpeed : -maxSpeed, rigid.velocity.y);
+
+        // Landing platform
+        if (rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+            RaycastHit2D raytHit = Physics2D.Raycast(rigid.position + (spriteRenderer.flipX ? Vector2.left : Vector2.right), Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (raytHit.collider != null)
+            {
+                if (raytHit.distance < 0.5f)
+                {
+                    Debug.Log(raytHit.collider.name);
+                    anim.SetBool("isJumping", false);
+                }
+            }
+        }
+
     }
 }
